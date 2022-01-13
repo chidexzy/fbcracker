34d0b29243c42803324b1a507f699674c63e1349
chidexzy <66396748+chidexzy@users.noreply.github.com>
2022-01-13 02:09:14 +0100

    Update fbcracker.py

--- fbcracker.py
+++ fbcracker.py
@@ -47,11 +47,11 @@ def logo():
 def main():
 	os.system("clear")
 	logo()
-	print("\t\033[93;1m      MAIN MENU 1.0.2\x1b[0m")
+	print("\t\033[93;1m      MAIN MENU\x1b[0m")
 	print("")
 	print("\033[92;1m  [1] START CRACK")
 	print("\033[93;1m  [2] HOW TO GET ACCESS TOKEN")
-	print("\033[94;1m  [3] UPDATE TOOL")
+	print("\033[94;1m  [3] UPDATE TOOL 1.1")
 	print("\033[96;1m  [4] CHAT ME UP ON WHATSAPP \033[92;1m✘\033[91;1m✘")
 	print("\033[90;1m  [0] EXIT")
 	print("")
